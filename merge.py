7e8282c983b3622550ec52713d642c26d6a9054e
a104590036 <t104590036@ntut.org.tw>
2018-06-01 14:22:16 +0800

    修改merge, one hot encoding

--- merge.py
+++ merge.py
@@ -2,6 +2,8 @@ import pandas as pd
 from functools import reduce
 import os
 article = pd.read_csv(".\\CleanArticle.csv", encoding = 'cp1252')
+article.pop('headline')
+article.pop('multimedia')
 articleData = pd.DataFrame(article)
 comment = pd.read_csv(".\\CleanComment.csv", encoding = 'cp850')
 comment.pop('commentBody')
@@ -10,12 +12,11 @@ comment.pop('typeOfMaterial')
 comment['recommendations']=comment['recommendations'].astype('int64')
 commentData = pd.DataFrame(comment)
 # 計算每一篇文章recommendations數量
-print ("-1")
 recommendationsData = commentData.groupby('articleID')[["recommendations"]].sum()
 # 計算每一篇文章有多少comments
-print ("-2")
 commentData = commentData.groupby('articleID').count()
 commentData["recommendations"] = recommendationsData
+commentData.columns = ['comment', 'recommendations']
 print (commentData)
 # articleData = articleData['typeOfMaterial'].unique()
 # articleData = articleData['typeOfMaterial'].unique()
---
+++ transfer.py
@@ -0,0 +1,5 @@
+import os
+import numpy as np
+import pandas as pd
+
+article = pd.read_csv(".\\one_hot_encoding.csv", encoding = 'cp1252')
